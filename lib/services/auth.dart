2cfea2074c6884c70506661d336dacf71d9f2393
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2022-10-31 23:55:40 +0600

    update

---
+++ lib/services/auth.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService{
+
+
+
+  //sign in anon
+
+  //sign in with email and password
+
+  //register with email and password
+
+  //sign out
+}
