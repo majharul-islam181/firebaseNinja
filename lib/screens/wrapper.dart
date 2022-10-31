f5a6c5efc6ad20a82d10dfd3d5e3ab641a7ad796
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2022-10-31 22:47:52 +0600

    update

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+
+
+    //either home or authenticate widget
+    return Container();
+  }
+}
